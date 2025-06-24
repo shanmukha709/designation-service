1975c2619295b5df41380675385db83c3f7173e9
Shanmukha Kadumuru <shanmukhak709@gmail.com>
2025-06-24 17:14:33 +0530

    Added frontend path

--- src/main/java/com/village/designation/controller/DesignationController.java
+++ src/main/java/com/village/designation/controller/DesignationController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {
+        "https://our-circle-ui.onrender.com",
+        "http://localhost:3000"
+})
 @RestController
 @RequestMapping("/designation")
 public class DesignationController {
